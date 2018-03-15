4268f68a46ed275a90d5052999ac082e4bdb2f5d
Kitty Davies <kittyfdavies@gmail.com>
2018-03-15 23:53:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,28 +1,30 @@
 # Write your code here.
 katz_deli = []
 
-def take_a_number(katz_deli, name)
+def take_a_number(deli, name)
   #katz_deli.each { |customer|
   #  puts "Welcome, #{customer}. You are number #{customer.index + 1} in line."}
+  name = name.split
+  deli << name
 end
 
-def line(katz_deli)
-  if katz_deli.count == 0
+def line(deli)
+  if deli.count == 0
     puts "The line is currently empty."
   else
     print "The line is currently: "
-    katz_deli.each { |name|
-      order = katz_deli.index + 1
-      puts "#{order}. #{katz_deli}"
+    deli.each { |name|
+      order = deli.index + 1
+      puts "#{order}. #{deli}"
     }
   end
 end
 
-def now_serving(katz_deli)
-  if katz_deli.count == 0
+def now_serving(deli)
+  if deli.count == 0
     puts "There is nobody waiting to be served!"
   else
-    current_customer = katz_deli.shift
+    current_customer = deli.shift
     puts "Currently serving #{current_customer}."
   end
 end
