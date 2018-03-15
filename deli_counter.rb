ef48a97a1acff9318e4fda9b7295344c042d5527
Kitty Davies <kittyfdavies@gmail.com>
2018-03-15 23:35:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,15 @@
 katz_deli = []
 
 def take_a_number(katz_deli, name)
-
+  katz_deli.each { |customer|
+    puts "Welcome, #{customer}. You are number #{customer.index + 1}"}
 end
 
 def line(katz_deli)
   if katz_deli.count == 0
     puts "The line is currently empty."
   else
-    
+
   end
 end
 
