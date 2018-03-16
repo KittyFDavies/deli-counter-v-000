6b03704ed1e218a391254d38200d37eeabb6009a
Kitty Davies <kittyfdavies@gmail.com>
2018-03-16 00:08:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,8 +10,9 @@ def line(deli)
   if deli.count == 0
     puts "The line is currently empty."
   else
+    puts "The line is currently: "
     deli.each { |name|
-      puts "The line is currently: #{deli.index(name) + 1}. #{name}"
+      puts "#{deli.index(name) + 1}. #{name}"
     }
   end
 end
